efabd1b3c96507b254d86d6c0222c7055692064b
mohammad <khezri@outlook.com>
2019-03-12 13:22:51 -0500

    chapter_17

--- src/components/Fish.js
+++ src/components/Fish.js
@@ -3,10 +3,6 @@ import { formatPrice } from '../helpers';
 
 class Fish extends React.Component {
 
-  handleClick = () => {
-    this.props.addToOrder(this.props.index);
-  };
-
   render() {
     const { image, name, price, desc, status } = this.props.details;
     const isAvailable = status === 'available';
@@ -18,10 +14,10 @@ class Fish extends React.Component {
           <span className="price">{formatPrice(price)}</span>
         </h3>
         <p>{desc}</p>
-        <button disabled={!isAvailable} onClick={this.handleClick}>
+        <button disabled={!isAvailable} onClick={() => this.props.addToOrder(this.props.index)}>
           {isAvailable ? 'Add To Order' : 'Sold out!'}
         </button>
-      </li>
+      </li >
     );
   }
 }
